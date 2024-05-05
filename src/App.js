4e43eee141fc50499cac204d23f393bea1654bbb
NGUYỄN THIỆN TÂM <thientam2829@gmail.com>
2024-05-05 14:56:59 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
